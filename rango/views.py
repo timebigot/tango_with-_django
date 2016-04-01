bcbaaa8e5d30846209492405522a32082be84f8a
timebigot <timebigot@gmail.com>
2016-03-31 20:24:38 -0400

    almost done with part 8

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -32,3 +34,41 @@ def category(request, category_name_slug):
         pass
     
     return render(request, 'rango/category.html',context_dict)
+    
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        if form.is_valid():
+            form.save(commit=True)
+            
+            return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+        
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+                
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+    else:
+        form = PageForm()
+        
+    context_dict = {'form':form, 'category': cat}
+    
+    return render(request, 'rango/add_page.html', context_dict)
