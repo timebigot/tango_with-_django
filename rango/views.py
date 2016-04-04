0ddf930e32cda3a20a5b42c9ac6c997c372e825b
timebigot <timebigot@gmail.com>
2016-04-04 15:50:56 -0400

    completed part 10

--- rango/views.py
+++ rango/views.py
@@ -21,7 +21,7 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says this is the about")
+    return render(request, 'rango/about.html', {})
     
 def category(request, category_name_slug):
     context_dict = {}
